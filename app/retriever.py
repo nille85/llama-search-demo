4e6879986db8eb82f72e7cc0855ec9a305787847
Niels <niels.holvoet@gmail.com>
2024-03-25 13:16:33 +0100

    added C4 example and added chat to documents

--- app/retriever.py
+++ app/retriever.py
@@ -1,9 +1,18 @@
+from llama_index.core import  VectorStoreIndex
+from llama_index.core.chat_engine.types import ChatMode
 class Retriever:
 
     def __init__(self, llm):
         self.llm = llm
 
-    def query(self, index, query):
+    def query(self, index: VectorStoreIndex, query):
         query_engine = index.as_query_engine(llm=self.llm)
         response = query_engine.query(query)
         return response
+
+    def chat(self, index: VectorStoreIndex, query):
+        chat_engine = index.as_chat_engine(llm=self.llm, chat_mode=ChatMode.CONDENSE_QUESTION)
+        response = chat_engine.chat(query)
+        return response
+
+    
--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from app.infrastructure import get_vector_database, get_configuration, get_vecto
 from app.retriever import Retriever
 import logging
 import sys
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+
+from rich.console import Console
+from rich.table import Table
+from rich.markdown import Markdown
+console = Console()
 
 
-
+def reply(response):
+    md = Markdown(response.response)
+    console.print(md)
+    table = Table(title=" Sources used")
+    table.add_column("Score", style="cyan")
+    table.add_column("Metadata", style="magenta")
+    for source_node in response.source_nodes:
+        table.add_row(str(source_node.score), str(source_node.node.metadata))
+    console.print(table)
 
 if __name__ == "__main__":
     config = get_configuration("dev_config.toml")
@@ -15,12 +28,23 @@ if __name__ == "__main__":
     #specify collection name from vector store, here it is 'genai'
     retriever_vector_store = get_vector_store(vector_db, "genai")
     index = get_vector_store_index(retriever_vector_store,embed_model)
+    
     llm_model = get_llm_model()
-
     retriever = Retriever(llm_model)
-    response = retriever.query(index, "Could you summarize the given context? Return your response which covers the key points of the text and does not miss anything important, please.")
+    #We retrieve a document, and summarize it.
+    response = retriever.query(index, "Can you summarize the given context? The response must covers the key points of the text in bullet point format and it must not miss anything important. The response must be returned in markdown format.")
+    reply(response)
 
-    print(response)
+    while True:
+        console.print("Starting chat mode")
+        user_input = input("Enter your input: ")
+        if user_input == "\\bye":
+            break
+        else:
+        # Process the user input here
+            response = retriever.chat(index, user_input)
+            reply(response)
+
 
     
 
